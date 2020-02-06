0642a920c8c9f6dc84c99506cb26f87be8f2f937
Karolina Kuchta <linka_94@interia.eu>
2020-02-06 12:16:01 +0100

    Divide AdminPanel

---
+++ src/components/AddBookForm.js
@@ -0,0 +1,113 @@
+import React from "react";
+import {fbase} from "../fbase";
+
+class AddBookForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            books: [],          
+            book: {
+                name: "",
+                author: "",
+                desc: "",
+                inStock: false,
+                image: ""
+            }
+        }
+    }
+
+    addNewBook = (event) => {
+        event.preventDefault();     
+        let newBook = { ...this.state.book };        
+
+        this.setState({  
+            books: [...this.state.books, newBook],          
+            book: {
+                name: "",
+                author: "",
+                desc: "",
+                inStock: false,
+                image: ""
+            }
+        });        
+    }
+
+    componentDidMount() {
+        if(localStorage.getItem("loggedIn")) {
+            this.setState({loggedIn: localStorage.getItem("loggedIn")});
+        }
+        this.ref = fbase.syncState('bookstore/books', {
+            context: this,
+            state: 'books',
+            asArray: true
+        });
+    }
+
+    componentWillUnmount() {
+        fbase.removeBinding(this.ref);
+    } 
+
+    handleChange = (event) => {
+        let newBook;
+        
+        if(event.target.name ==="inStock") {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.checked
+            };
+        } else {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.value
+            };
+        }
+        
+        this.setState({
+            book: newBook
+        });
+    }
+
+    render() {
+        return (
+            <div className="adminPanel col-md-4">
+                <form onSubmit={this.addNewBook}>
+                    <div className="form-group">
+                        <label htmlFor="name" className="label">Book name : 
+                            <input type="text" placeholder="Book name" id="name" name="name" className="form-control" value={this.state.book.name} onChange={this.handleChange} />
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="author" className="label">Book author : 
+                            <input type="text" placeholder="Book author" id="author" name="author" className="form-control" value={this.state.book.author} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="desc" className="label">Book description : 
+                            <textarea type="text" placeholder="Book description" id="desc" name="desc" className="form-control" value={this.state.book.desc} onChange={this.handleChange}/>
+                        </label>
+                    </div>
+                    <div className="form-group">
+                        <input type="checkbox" id="inStock" name="inStock" className="form-check-input" value={this.state.book.inStock} onChange={this.handleChange} />
+                        <label htmlFor="inStock" className="form-check-label">in stock</label>                       
+                    </div> 
+                    <div className="form-group">
+                        <div className="input-group">
+                          <div className="input-group-prepend">
+                            <span className="input-group-text" id="inputGroupFileAddon01">Upload</span>
+                          </div>
+                          <div className="custom-file">
+                            <input type="text" className="custom-file-input" id="inputGroupFile01"
+                              aria-describedby="inputGroupFileAddon01" name="image" value={this.state.book.image} onChange={this.handleChange} />
+                            <label className="custom-file-label" htmlFor="inputGroupFile01">Choose image</label>
+                          </div>
+                        </div>                      
+                            {/* <input type="text" placeholder="Book image" id="image" name="image" className="form-control" value={this.state.book.image} onChange={this.handleChange} />                        */}
+                    </div>  
+                    <button type="submit" className="btn btn-secondary addBook">Add Book</button>                     
+                </form>                
+            </div>
+        );
+    }
+}
+
+export default AddBookForm;
---
+++ src/components/LoginPanel.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {firebaseApp} from "../fbase";
+
+class LoginPanel extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    auth = (event) => {
+        event.preventDefault();
+        firebaseApp.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then( () => {
+                this.props.changeLoggedIn(true);
+                localStorage.setItem("loggedIn", true);
+            })
+            .catch( () => {
+                console.log('Unable to authenticate');
+            })        
+    }
+
+    handleLoginChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.auth} className="logInForm">
+                <label htmlFor="email" className="label">Email :
+                    <input type="text" placeholder="email" id="email" name="email" className="form-control"  value={this.state.email} onChange={this.handleLoginChange} />
+                </label>
+                <label htmlFor="password" className="label">Password :
+                    <input type="password" placeholder="password" id="password" name="password" className="form-control" value={this.state.password} onChange={this.handleLoginChange} />
+                </label>
+                <button type="submit" className="btn btn-success">Log in</button>
+            </form>
+        );
+    }
+}
+
+export default LoginPanel;
